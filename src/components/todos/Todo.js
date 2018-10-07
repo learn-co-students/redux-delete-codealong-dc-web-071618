45793c5fd69d4aa9817d9324fccca12c2c238411
julianajlk <julianajlk@gmail.com>
2018-10-07 14:55:37 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Todo = props => <li>{props.text}</li>
+const Todo = props => {
+  //2. Add DELETE button and onClick
+  //6. update delete to have id as payload
+  return (
+    <div>
+      <span>
+        <li>{props.todo.text}</li>
+      </span>
+      <button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+    </div>
+  );
+};
 
 export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,26 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
+export default function manageTodo(
+  state = {
+    todos: []
+  },
+  action
+) {
+  console.log(action);
   switch (action.type) {
-    case 'ADD_TODO':
+    case "ADD_TODO":
+      // return { todos: state.todos.concat(action.payload.text) };
 
-      return { todos: state.todos.concat(action.payload.text) };
+      //create an ID for each todo
+      const todo = {
+        id: Math.random() * 10000000000000000,
+        text: action.payload.text
+      };
+      return { todos: state.todos.concat(todo) };
+
+    //4. Add DELETE_TODO --> filter that returns every todo that doesn't match what is contained in action.payload
+
+    //7. update reducer to have id since it's being passed as props from todo.js
+    case "DELETE_TODO":
+      return { todos: state.todos.filter(todo => todo.id !== action.payload) };
 
     default:
       return state;
